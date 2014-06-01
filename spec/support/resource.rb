13348617d9f507bb761b78b45ba4c9069b887654
Mathias Söderberg <mathias.soederberg@gmail.com>
2014-06-01 17:32:46 +0200

    Simplify Resource (only used for tests)
    Keep it cool with some good ol’ Ruby 1.9.3 syntax.

--- spec/support/resource.rb
+++ spec/support/resource.rb
@@ -1,25 +1,10 @@
 # encoding: utf-8
 
 class Resource
-  TYPES = %i[instances]
-
-  class << self
-    def [](type)
-      @resources ||= {}
-      @resources[type] ||= JSON.parse(read_file(type), symbolize_names: true)
-    end
-
-    TYPES.each do |type|
-      define_method(type) { self[type] }
-    end
-
-    private
-
-    def read_file(type)
-      path = File.expand_path("../../resources/#{type}.json", __FILE__)
-      raise ArgumentError, "Unable to load #{path}" unless File.exists?(path)
-
-      File.read(path)
+  def self.instances
+    @instances ||= begin
+      path = File.expand_path(%(../../resources/instances.json), __FILE__)
+      JSON.parse(File.read(path), symbolize_names: true)
     end
   end
 end
